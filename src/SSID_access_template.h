e19341db0b27a72bdbbd0ed18ed456bb54976246
Chris Klomp <linux@klomp.ca>
2024-12-23 09:16:24 -0500

    comments in template

--- src/SSID_access_template.h
+++ src/SSID_access_template.h
@@ -2,6 +2,6 @@
 // And fill in your relevant values
 #define MY_SSID "<your-network>"
 #define SSID_PSW "<your-password>"
-#define OTA_PSW "HOotaIdentifier"      // This is to make sure you don't update the wrong ESP by accident.
-#define mqtt_user "<mqtt-user>"
-#define mqtt_password "<mqtt-password>"
+#define OTA_PSW "HOotaIdentifier"		  // This is to make sure you don't update the wrong ESP by accident.
+#define MQTT_USER "<mqtt-user>"
+#define MQTT_PASSWORD "<mqtt-password>"
